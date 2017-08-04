68e1dcd1a003e61ca4b55338ce89598ee5482a10
DEV3L <jus.beall@gmail.com>
2017-08-04 00:08:06 -0400

    Refactor add_item_to_temp_mapping to use defaultdict

--- twitter_sentence_generator/builders/mapping_builder.py
+++ twitter_sentence_generator/builders/mapping_builder.py
@@ -1,10 +1,12 @@
+from collections import defaultdict
+
 from twitter_sentence_generator.transformers.list_transformer import ListTransformer
 
 
 class MappingBuilder():
     def __init__(self, mapping=None, temp_mapping=None, starts=None):
         self.mapping = mapping or {}
-        self.temp_mapping = temp_mapping or {}
+        self.temp_mapping = temp_mapping or defaultdict(lambda: defaultdict(int))
         self.starts = starts or []
 
     def add_item_to_temp_mapping(self, history, word):
@@ -21,16 +23,7 @@ class MappingBuilder():
         """
         while history:
             first = ListTransformer(history).tuple
-
-            # this could be shortened up with a default dict
-            if first in self.temp_mapping:
-                if word in self.temp_mapping[first]:
-                    self.temp_mapping[first][word] += 1.0
-                else:
-                    self.temp_mapping[first][word] = 1.0
-            else:
-                self.temp_mapping[first] = {}
-                self.temp_mapping[first][word] = 1.0
+            self.temp_mapping[first][word] += 1.0
             history = history[1:]
 
         return self.temp_mapping
